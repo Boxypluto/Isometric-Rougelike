4f22f38d594a6256ba79293f2848db09a66b4fd2
Boxypluto <122708916+Boxypluto@users.noreply.github.com>
2024-04-19 14:51:33 -0400

    First Area Started

--- Scenes/Room.gd
+++ Scenes/Room.gd
@@ -4,6 +4,7 @@ class_name Room
 @export var EnemiesDict : Dictionary
 @onready var EnemiesNode = $"Y-Sortables/Enemies"
 
+const DEBUG_MODE = true
 
 func _ready():
 	var EnemyGroupChildren : Array = EnemiesNode.get_children()
@@ -14,5 +15,5 @@ func _ready():
 			EnemyGroupChildren[index].RoomNode = self
 
 func _process(delta):
-	if EnemiesDict.is_empty():
+	if EnemiesDict.is_empty() and not DEBUG_MODE:
 		GameManager.ProgressRooms(self)
