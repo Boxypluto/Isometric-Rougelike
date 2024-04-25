1cc2feebcaf731d857c32a8cbc201f2af16609c5
Boxypluto <122708916+Boxypluto@users.noreply.github.com>
2024-04-25 11:55:02 -0400

    Attack Rework Started

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,22 +7,11 @@ const ATTACKING_WALK_SPEED = 96/2
 # The HurtBox's Shape for the Player
 @onready var hurt_box_shape = $HurtBoxComponent/HurtBoxShape
 
-# The Spell Scene
-const PLAYER_SPELL = preload("res://Player/PlayerSpell.tscn")
-
 # Frames, counts the physics frames
 var frames : int
 
 # Instantiated Node
 @onready var instantiated = $"../Instantiated"
-
-# The player's attack node and sprite
-@onready var attack : AnimatedSprite2D = $Attack
-
-# The Area2D of the player's attack
-@onready var attack_area : Area2D = $Attack/AttackArea
-# The CollisionShape of the player's attack
-@onready var attack_shape : CollisionShape2D = $Attack/AttackArea/AttackShape
 
 # Components
 @onready var health : HealthComponent = $HealthComponent
@@ -97,32 +86,9 @@ func _physics_process(_delta):
 	
 	move_and_slide()
 	
-	if Input.is_action_pressed("Attack") and frames % 10 == 0 and PlayerCanShoot:
-		var spell : Node = PLAYER_SPELL.instantiate()
-		add_child(spell)
-		spell.rotation = attack.global_position.angle_to_point(get_global_mouse_position())
-		spell.global_position = attack.global_position
-		spell.StartVelocity = Vector2.ZERO
-		#SuccessfullMelleAttack(spell.rotation)
 
 func _process(_delta):
-	
 	var MousePos = get_global_mouse_position()
-	
-	var attackAngle = attack.global_position.angle_to_point(MousePos)
-	
-	#region SCRAPPED ATTACK
-	
-	if not attack.animation == "Attack" and false:
-		attack.rotation = attackAngle
-	
-	if attack.animation == "Attack" and attack.frame == 3 and false:
-		attack.animation = "Nothing"
-		Attacking = false
-		attack_shape.disabled = true
-	
-#endregion
-	
 
 func _input(event):
 	
@@ -133,32 +99,12 @@ func _input(event):
 		PlayerCanShoot = false
 		PlayerDashDirection = direction
 		hurt_box_shape.disabled = true
-		
-	
-	##region SCRAPPED_ATTACK
-	#
-	#if event.is_action_pressed("Attack") and false:
-		#attack.animation = "Attack"
-		#Attacking = true
-		#attack_shape.disabled = false
-##endregion
-	#
-	#if event.is_action("Attack"):
-		#var spell : Node = PLAYER_SPELL.instantiate()
-		#instantiated.add_child(spell)
-		#spell.rotation = attack.global_position.angle_to_point(get_global_mouse_position())
-		#spell.global_position = attack.global_position
-		#SuccessfullMelleAttack(spell.rotation)
-
-func SuccessfullMelleAttack(angle = attack.rotation):
-	RecoilVelocity = Vector2.from_angle(angle + PI) * RecoilStrength
 
 func player_to_mouse_angle():
 	return (global_position - Vector2(0, 8)).angle_to_point(get_global_mouse_position())
 
 func player_to_mouse_vector():
 	return Vector2.from_angle(player_to_mouse_angle())
-
 
 func PlayerIsHit(damage):
 	camera.cause_shake(2)
