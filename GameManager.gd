fd46b0305220ffa359e4c9fa4c335b57d1ffa2a6
Boxypluto <122708916+Boxypluto@users.noreply.github.com>
2024-05-01 12:59:16 -0400

    World Generator

--- GameManager.gd
+++ GameManager.gd
@@ -10,15 +10,34 @@ var TestingRooms : Array = [
 
 var Area1 : Array = [
 	preload("res://Scenes/Areas/Area 1/Archipelago.tscn"),
-	preload("res://Scenes/Areas/Area 1/AtopTheWall.tscn"),
-	preload("res://Scenes/Areas/Area 1/GrassIslands.tscn")
+	preload("res://Scenes/Areas/Area 1/FortressWall.tscn"),
+	preload("res://Scenes/Areas/Area 1/GrassIslands.tscn"),
+	preload("res://Scenes/Areas/Area 1/LoopAround.tscn"),
+	preload("res://Scenes/Areas/Area 1/Sparse.tscn")
+]
+
+var Area2 : Array = [
+	preload("res://Scenes/Areas/Area 2/Arena.tscn"),
+	preload("res://Scenes/Areas/Area 2/SpikePit.tscn"),
+	preload("res://Scenes/Areas/Area 2/ThinPeak.tscn"),
+	preload("res://Scenes/Areas/Area 2/ValleyRidge.tscn"),
+	preload("res://Scenes/Areas/Area 2/ZigZag.tscn")
+]
+
+var Area3 : Array = [
+	preload("res://Scenes/Areas/Area 3/BioSpheres.tscn"),
+	preload("res://Scenes/Areas/Area 3/LonelyStation.tscn"),
+	preload("res://Scenes/Areas/Area 3/Satellites.tscn"),
+	preload("res://Scenes/Areas/Area 3/Ships.tscn"),
+	preload("res://Scenes/Areas/Area 3/Spread.tscn")
 ]
 
 var AreaLevelCount : int = 3
 
 var Areas : Dictionary = {
-	#"Testing" : TestingRooms,
-	"Area1" : Area1
+	"Area1" : Area1,
+	"Area2" : Area2,
+	"Area3" : Area3
 }
 
 var WorldDictionary : Dictionary = {}
@@ -33,12 +52,13 @@ func GenerateRooms():
 	WorldDictionary.clear()
 	AreaArray.clear()
 	
-	for area in Areas:
+	for area in range(len(Areas)):
 		
 		var AreaDictionary : Dictionary
 		var LevelList : Array
-		for scene in Area1:
+		for scene in Areas.values()[area]:
 			LevelList.append(scene)
+			
 		
 		WorldDictionary[area] = { }
 		
@@ -66,7 +86,10 @@ func StartGame(scene_to_remove = null):
 
 func ProgressRooms(scene_to_remove = null):
 	
+	if not CurrentAreaIndex < 3:
+		print("CONGRADULATIONS YOU FOUND ALL 7 NOTEBOOKS!!!!!!!!!!!!!!!!!!!!?!?!")
 	if CurrentRoomIndex == len(WorldDictionary.values()[CurrentAreaIndex]) - 1:
+		CurrentAreaIndex += 1
 		CurrentRoomIndex = 0
 	else:
 		CurrentRoomIndex += 1
