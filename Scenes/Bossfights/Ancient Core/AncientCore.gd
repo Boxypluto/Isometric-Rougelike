67b127457618b3f74c8621bde79c126dfb930696
Boxypluto <122708916+Boxypluto@users.noreply.github.com>
2024-05-14 12:52:35 -0400

    Some Commenting, Icon Work, and Export Presets Changes

--- Scenes/Bossfights/Ancient Core/AncientCore.gd
+++ Scenes/Bossfights/Ancient Core/AncientCore.gd
@@ -47,16 +47,20 @@ var NormalSpeed : float = 1.5
 var LaserSpeed : float = 0.5
 
 func _process(delta):
+	# Set the speed of the two rotating layers to whatever the speed is
 	layer.Speed = Speed
 	layer_2.Speed = -Speed
 	
+	# If the CORE is shooting lasers or rifts, slow down the CORE to LaserSpeed, otherwise, go NormalSpeed
 	if Lasering or Rifting:
 		Speed = LaserSpeed
 	else:
 		Speed = NormalSpeed
 	
+	# Rotate the turret to face the player
 	turret.rotation = turret.global_position.angle_to_point(player.global_position) - PI/2
 	
+	# Set the health bar's value to the CORE's health
 	health_bar.value = clamp(health.Health, 0, 64000000)
 
 func _ready():
@@ -89,45 +93,83 @@ func _ready():
 	laser_timer.start()
 	rift_timer.start()
 
+# Used to know if the laser attack is currently active
 var Lasering : bool = false
+# Actived by the laser_timer Node
 func LaserTimer():
+	# Start the laser charge noise
 	laser_charge.play()
+	# Enable Lasering
 	Lasering = true
+	# For each laser...
 	for laser in Lasers:
+		# Make it visible
 		laser.visible = true
+		# Make it very transparent
 		laser.modulate.a = 0.2
+	
+	# Wait for 2 seconds
 	await get_tree().create_timer(2).timeout
+	# Stop the laser charge sound
 	laser_charge.stop()
+	# Start the laser shoot sound
 	laser_shoot.play()
+	# For each laser...
 	for laser in Lasers:
+		# Activate the Laser's Damaging Collision
 		laser.get_child(0).get_child(0).disabled = false
+		# Make the Laser fully visible
 		laser.modulate.a = 1
+		
+	# Wait 2 seconds
 	await get_tree().create_timer(2).timeout
+	# Stop the laser shoot sound
 	laser_shoot.stop()
+	# For each laser...
 	for laser in Lasers:
+		# Make it invisible
 		laser.visible = false
+		# Disable its Damagine Collision
 		laser.get_child(0).get_child(0).disabled = true
+	# Set lasering to false
 	Lasering = false
 
+# Used to know if the rift attack is currently active
 var Rifting : bool = false
 func RiftTimer():
+	# Set rifting to true
 	Rifting = true
+	# Play the rift open sound
 	rift_open.play()
-	print("RIFT")
+	# Increment RiftIndex
 	RiftIndex += 1
+	# If RiftIndex is going to become out of range...
 	if RiftIndex == len(Rifts):
+		# Set rift index to zero
 		RiftIndex = 0
+	# Get the correct rift using RiftIndex on Rifts
 	var rift : Sprite2D = Rifts[RiftIndex]
+	# Get the Damaging Collision of the Rift and set it to col
 	var col : CollisionPolygon2D = rift.get_child(0).get_child(0)
+	# Make the rift semi-transparent
 	rift.modulate.a = 0.1
+	# Make the rift visible
 	rift.visible = true
+	
+	# Wait 4 seconds
 	await get_tree().create_timer(4).timeout
+	# Make the rift fully visible
 	rift.modulate.a = 1
+	# Activate the Damaging Collison of the rift
 	col.disabled = false
+	
+	# Wait 3 seconds
 	await get_tree().create_timer(3).timeout
+	# Set the rift to invisible
 	rift.visible = false
+	# Disable the Damaging Collision of the rift
 	col.disabled = true
-
+	# Set rifting to false
 	Rifting = false
 
 func TurretTimer():
